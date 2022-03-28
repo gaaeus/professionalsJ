3107522fe4884d009696c0a1734109a0b375d120
Helio Silva <helio.silva@handelsbanken.co.uk>
2022-03-28 11:08:39 +0100

    Serialization, getters and setters for Rank.

--- models/Rank.java
+++ models/Rank.java
@@ -1,12 +1,14 @@
 package models;
 
+import java.io.Serializable;
+
 /** Rank class with the user and professional's identifiers, along with the value set 
  *  
  * @author  Hélio Silva
  * @version 1.0
  * @since   2022-03-28
  */
-public class Rank extends Base {
+public class Rank extends Base implements Serializable {
 
     protected int UserId;
 
@@ -18,4 +20,36 @@ public class Rank extends Base {
 
     /** Gets or sets the rank for the professional */
     protected int RankValue;
+
+    public int getUserId() {
+        return this.UserId;
+    }
+
+    public void setUserId(int UserId) {
+        this.UserId = UserId;
+    }
+
+    public int getProfessionalID() {
+        return this.ProfessionalID;
+    }
+
+    public void setProfessionalID(int ProfessionalID) {
+        this.ProfessionalID = ProfessionalID;
+    }
+
+    public int getCommentID() {
+        return this.CommentID;
+    }
+
+    public void setCommentID(int CommentID) {
+        this.CommentID = CommentID;
+    }
+
+    public int getRankValue() {
+        return this.RankValue;
+    }
+
+    public void setRankValue(int RankValue) {
+        this.RankValue = RankValue;
+    }
 }
